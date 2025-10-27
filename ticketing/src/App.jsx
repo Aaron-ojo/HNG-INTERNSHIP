13f4dabb78173622f94b0eb21ab6a0e32ff98ce3
Aaron-ojo <168048069+Aaron-ojo@users.noreply.github.com>
2025-10-27 14:06:39 -0700

    changed to hashrouter

--- ticketing/src/App.jsx
+++ ticketing/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
@@ -8,15 +8,15 @@ import Ticketmanagement from "./pages/Ticketmanagement";
 function App() {
   return (
     <div className="max-h-[1440px]">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/ticketmanagement" element={<Ticketmanagement />} />
+          <Route path="/#/" element={<LandingPage />} />
+          <Route path="/#/login" element={<Login />} />
+          <Route path="/#/dashboard" element={<Dashboard />} />
+          <Route path="/#/signup" element={<Signup />} />
+          <Route path="/#/ticketmanagement" element={<Ticketmanagement />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
